9b832692ef16d97446f7d125043a87896786f1a5
adamus.r <adamus.r@gmail.com>
2022-04-10 19:14:05 +0200

    lbkata_test6red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -73,6 +73,16 @@ class ServerLoadBalancerTest {
 
     }
 
+    @Test
+    void balanceAServerWithNotEnoughRoom_shouldNotBeFilledWithAVm() {
+        Server theServer = a(server().withCapacity(10)
+                                     .withCurrentLoadOf(90.0d));
+        Vm theVm = a(vm().ofSize(2));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+        assertThat("the server should not contain vm", !theServer.contains(theVm));
+    }
+
     private Vm[] aListOfVmsWith(Vm... vms) {
         return vms;
     }
