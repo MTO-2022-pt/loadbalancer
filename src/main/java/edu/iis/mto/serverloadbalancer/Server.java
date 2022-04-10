3fbfe454d3268e39907a13f6276793824b1c00d0
adamus.r <adamus.r@gmail.com>
2022-04-10 17:53:38 +0200

    lbkata_test3refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,17 +1,21 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 public class Server {
 
-	public double currentLoadPecentage;
-	public int capacity;
+    private static final double MAXIMUM_LOAD = 100.0d;
+    public double currentLoadPecentage;
+    private int capacity;
 
-	public Server(int capacity) {
-		this.capacity = capacity;
-	}
+    public Server(int capacity) {
+        this.capacity = capacity;
+    }
 
-	public boolean contains(Vm vm) {
-		return true;
-	}
+    public boolean contains(Vm vm) {
+        return true;
+    }
+
+    public void addVm(Vm vm) {
+        this.currentLoadPecentage = (double) vm.size / (double) this.capacity * MAXIMUM_LOAD;
+    }
 
 }
