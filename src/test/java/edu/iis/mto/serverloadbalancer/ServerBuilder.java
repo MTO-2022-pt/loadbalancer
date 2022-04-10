2f44ca05937ec2c13008bbb456921d4041e76b2d
adamus.r <adamus.r@gmail.com>
2022-04-10 19:11:54 +0200

    lbkata_test5refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -13,13 +13,17 @@ public class ServerBuilder implements Builder<Server> {
     @Override
     public Server build() {
         Server server = new Server(capacity);
+        addInitialLoad(server);
+        return server;
+    }
+
+    private void addInitialLoad(Server server) {
         if (initialLoad > 0) {
-            int expectedLoad = (int) (initialLoad / 100.0d * server.getCapacity());
+            int expectedLoad = (int) (initialLoad / Server.MAXIMUM_LOAD * server.getCapacity());
             server.addVm(VmBuilder.vm()
                                   .ofSize(expectedLoad)
                                   .build());
         }
-        return server;
     }
 
     public static ServerBuilder server() {
