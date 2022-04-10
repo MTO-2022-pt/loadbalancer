480745dbecfc0e36f086ffa399a41db4e23f76e7
adamus.r <adamus.r@gmail.com>
2022-04-10 11:58:21 +0200

    lbkata_test1red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -12,4 +12,12 @@ class ServerLoadBalancerTest {
         assertThat(true, equalTo(true));
     }
 
+    @Test
+    void balancingAServer_noVms_serverStaysEmpty() {
+        Server theServer = a(server().withCapacity(1));
+
+        balance(aListOfServersWith(theServer), anEmptyListOfVms());
+
+        assertThat(theServer, hasLoadPercentageOf(0.0d));
+    }
 }
