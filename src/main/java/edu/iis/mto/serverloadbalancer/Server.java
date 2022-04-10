fbc998d476b6b28e844fa9b906b32a89daafa127
adamus.r <adamus.r@gmail.com>
2022-04-10 13:11:52 +0200

    lbkata_test3green

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,13 +1,17 @@
 package edu.iis.mto.serverloadbalancer;
 
+
 public class Server {
 
-    public double currentLoadPecentage;
+	public double currentLoadPecentage;
+	public int capacity;
 
-    public Server(int capacity) {}
+	public Server(int capacity) {
+		this.capacity = capacity;
+	}
 
-    public boolean contains(Vm vm) {
-        return true;
-    }
+	public boolean contains(Vm vm) {
+		return true;
+	}
 
 }
